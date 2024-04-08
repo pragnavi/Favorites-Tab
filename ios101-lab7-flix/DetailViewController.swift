f402d5246f97589c0c4bc0aec81793e32dcd3f4a
pragnavi <pragnavi1398@gmail.com>
2024-04-08 01:19:40 -0400

    Updated Favorite Movies when the User Toggles the Favorite Button

--- ios101-lab7-flix/DetailViewController.swift
+++ ios101-lab7-flix/DetailViewController.swift
@@ -23,6 +23,15 @@ class DetailViewController: UIViewController {
     @IBAction func didTapFavoriteButton(_ sender: UIButton) {
         // Set the button's isSelected state to the opposite of it's current value.
         sender.isSelected = !sender.isSelected
+        // 1. If the button is in the *selected* state (i.e. "favorited") -> Add movie to favorites
+        // 2. Otherwise, the button is in the *un-selected* state (i.e."un-favorited") -> Remove movie from favorites
+        if sender.isSelected {
+            // 1.
+            movie.addToFavorites()
+        } else {
+            // 2.
+            movie.removeFromFavorites()
+        }
     }
     var movie: Movie!
 
@@ -31,7 +40,23 @@ class DetailViewController: UIViewController {
 
         // TODO: Update favorite button selected state
 
+        // Update the button's selected state based on the current movie's favorited status.
+        // 1. Get the array of favorite movies.
+        // 2. Check if the favorite movies array contains the current movie.
+        // 3. If so, the movie has been favorited -> Set the button to the *selected* state.
+        // 4. Otherwise, the movie is not-favorited -> Set the button to the *un-selected* state.
+        // ------
 
+        // 1.
+        let favorites = Movie.getMovies(forKey: Movie.favoritesKey)
+        // 2.
+        if favorites.contains(movie) {
+            // 3.
+            favoriteButton.isSelected = true
+        } else {
+            // 4.
+            favoriteButton.isSelected = false
+        }
 
 
         // MARK: Style views
@@ -51,6 +76,8 @@ class DetailViewController: UIViewController {
 
         // Set the button's corner radius to be 1/2  it's width. This will make a square button round.
         favoriteButton.layer.cornerRadius = favoriteButton.frame.width / 2
+        
+        
         
         // Unwrap the optional vote average
         if let voteAverage = movie.voteAverage {
